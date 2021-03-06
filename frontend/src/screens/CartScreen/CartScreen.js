b638e28105c2c7af6b6a0f67f03f918b3720c3e9
alex <konichekalex@gmail.com>
2021-03-06 17:07:31 +0200

    improve select quantity markup in cartScreen

--- frontend/src/screens/CartScreen/CartScreen.js
+++ frontend/src/screens/CartScreen/CartScreen.js
@@ -1,11 +1,9 @@
-
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
-import Message from '../../components/Message';
-import { addToCart, removeFromCart } from '../../actions/cartActions';
-
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../../components/Message'
+import { addToCart, removeFromCart } from '../../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
@@ -52,7 +50,7 @@ const CartScreen = ({ match, location, history }) => {
                   </Col>
                   <Col md={2}>${item.price}</Col>
                   <Col md={2}>
-                    <Form.Control
+                    <Form.Control className="p-1"
                       as='select'
                       value={item.qty}
                       onChange={(e) =>
@@ -113,4 +111,4 @@ const CartScreen = ({ match, location, history }) => {
   )
 }
 
-export default CartScreen;
+export default CartScreen
